77521d8882e1e9b876e1d383224d466eed31c49c
raphtrooper <raphmangin88@gmail.com>
2023-02-16 13:19:26 +0100

    Ajout des images et du catalogue

--- src/action/CatalogueAction.php
+++ src/action/CatalogueAction.php
@@ -16,7 +16,7 @@ class CatalogueAction extends Action {
                 </form></div>';
         $res.= "<div class=\"group-produit\">";
         while ($row = $rep->fetch()){
-             $res.="<div class='item-produit'><a href=?action=produit&id=".$row[0]."><img class='img-produit' src='image/$row[11]'></a><a class=\"group-produit\" href=?action=produit&id=".$row[0].">".$row[1]."<br>Prix : $row[3]</a></div>";
+             $res.="<div class='item-produit'><a href=?action=produit&id=".$row[0]."><img class='img-produit' src='image/$row[11]'></a><a class=\"group-produit\" href=?action=produit&id=".$row[0].">".$row[2]."<br>Prix : $row[3]</a></div>";
         }
         return $res."</div></HTML>";
     }
