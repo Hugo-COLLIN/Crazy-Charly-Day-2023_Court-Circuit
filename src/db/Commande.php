a9ec50fc583567c0e67c1bfd2b7e0490ad3108f2
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-02-16 16:38:16 +0100

    8. Liste Commandes produits

--- src/db/Commande.php
+++ src/db/Commande.php
@@ -8,33 +8,50 @@ class Commande
     public static function afficherListe(): string {
         $res = "";
         $bd = ConnectionFactory::makeConnection();
-        $query = $bd->prepare("select * from commande /*NATURAL JOIN userccd*/");
+        $query = $bd->prepare("select * from commande NATURAL JOIN userccd");
         $query->execute();
 
         $res.= "<section class=\"liste-commandes\">";
         while ($row = $query->fetch()){
             $res .= "<div class='item-commande'>";
-            $res.="<div class='elt-item-commande'>Id : ".$row[1]."</div>";
+            $res.="<h4>".$row[5]."</h4>";
             $res.="<div class='elt-item-commande'>Date : ".$row[2]."</div>";
-            $res.="<div class='elt-item-commande'>Valeur : ".$row[3]."</div>";
+            $res.="<div class='elt-item-commande'>Valeur : ".$row[3]."€</div>";
+            $res .= self::afficherListeProduit($row[0]);
             $res .= "</div>";
         }
         $res.= "</section>";
-/*
-        foreach (Commande::getAll() as $commande) {
-            $res .= $query->dateCommande;
-            //$res .= $query->
-        }
-            $res .= "<td>" . $query->dateCommande . "</td>";
-            $res .= "<td>" . $commande->getDateCommande() . "</td>";
-            $res .= "<td>" . $commande->getMontant() . "</td>";
-            $res .= "<td>" . $commande->getEtat() . "</td>";
-            $res .= "<td>";
-            foreach ($commande->getProduits() as $produit) {
-                $res .= "<div class='item-produit'><a href=?action=produit&id=" . $produit->getId() . "><img class='img-produit' src='image/" . $produit->getImage() . "'></a><a class=\"group-produit\" href=?action=produit&id=" . $produit->getId() . ">" . $produit->getTitre() . "<br>Prix : " . $produit->getPrix() . "</a></div>";
-            }
-            $res .= "</td>";
-            $res .= "</tr>";*/
         return $res;
     }
+
+    private static function afficherListeProduit(int $int)
+    {
+        $res = "";
+        $bd = ConnectionFactory::makeConnection();
+        $query = $bd->prepare("select * from produit INNER JOIN contient where contient.idcommande = ? and contient.numProduit = produit.id");
+        $query->bindParam(1, $int);
+        $query->execute();
+        $res .= "<div class='item-produit-commande'>";
+        while ($row = $query->fetch()){
+            $res.="<h5 class='elt-item-commande'>".$row[2]."</h5>";
+            $res .= "<div class='elt-item-commande'>Prix : ".$row[3]."€</div>";
+        }
+        $res .= "</div>";
+        return $res;
+    }
+
+    /*
+    private static function afficherListeProduit(int $int)
+    {
+        $res = [];
+        $bd = ConnectionFactory::makeConnection();
+        $query = $bd->prepare("select * from produit INNER JOIN contient where contient.idcommande = ? and contient.numProduit = produit.id");
+        $query->bindParam(1, $int);
+        $query->execute();
+        while ($row = $query->fetch()){
+            $res[] = $row[2];
+        }
+        return $res;
+    }
+    */
 }
