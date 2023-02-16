ba29751ee8d9a8d9cc9e683206869068ea39c621
alennardos <92914160+alennardos@users.noreply.github.com>
2023-02-16 13:08:53 +0100

    produit

---
+++ src/Produit.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace iutnc\ccd\produit;
+
+use iutnc\ccd\db\ConnectionFactory;
+use PDOException;
+
+class Produit
+{
+
+    public static function afficher(string $id): string {
+        $res = "";
+        $bd = ConnectionFactory::makeConnection();
+        $query = $bd->prepare("select * from produit where id = ?");
+        $query->bindParam(1, $id);
+        $query->execute();
+        $row = $query->fetch();
+        $res .= "<div class=\"description\"><p>". $row[2];
+
+        if ($row[4] == 0){
+            $res .= "prix au kilo : " . $row[3];
+        }else{
+            $res .= "prix : " . $row[3] . " poids :". "$row[4]";
+        }
+        $res .= "</p><p>description : " . $row[5] . "</p><p>Detail : " . $row[6] . "</div></p><p>Leux de production : " . $row[7] . "</p><p>Distance de Nancy : " . $row[8] . " coordonnees : ".$row[9].$row[10].$row[11]."</div>";
+
+        $res .= '<img src="img/'.$row[12].'">';
+        $res .= "
+        <form method='post' action='?action=ajout-comm&id=" . $id . "' class='comment-form'>
+        <h2>Note</h2>
+        <select name='note'>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option></select>
+        <h2>Commentaire</h2>
+        <textarea maxlength='520' name='commentaire' placeholder='Commentaire' required></textarea><br>
+        <button type='submit'>Envoyer</button></form><a href=?action=produit&id=" . $id . ">Retour</a>";
+        return $res;
+    }
+}
--- src/action/CatalogueAction.php
+++ src/action/CatalogueAction.php
@@ -6,20 +6,19 @@ use iutnc\ccd\db\ConnectionFactory;
 
 class CatalogueAction extends Action {
 
-    public function execute(): string{
+    public function execute(): string
+    {
         $res = "<HTML>";
         $bd = ConnectionFactory::makeConnection();
         $rep = $bd->query("select * from serie");
-        $res.= '<div style="margin: inherit;"><form action="?action=catalogue" method="post" class="search-form">
+        $res .= '<div style="margin: inherit;"><form action="?action=catalogue" method="post" class="search-form">
                     <label>Rechercher </label><input name="chaine">
                     <button type="submit">Validate</button>
                 </form></div>';
-        $res.= "<div class=\"series\">";
-        while ($row = $rep->fetch()){
-             $res.="<div class='video-div'><a href=?action=serie&id=".$row[0]."><img class='img-video' src='image/$row[3]'></a><a class=\"serie\" href=?action=serie&id=".$row[0].">".$row[1]."</a></div>";
+        $res .= "<div class=\"series\">";
+        while ($row = $rep->fetch()) {
+            $res .= "<div class='video-div'><a href=?action=serie&id=" . $row[0] . "><img class='img-video' src='image/$row[3]'></a><a class=\"serie\" href=?action=serie&id=" . $row[0] . ">" . $row[1] . "</a></div>";
         }
-        return $res."</div></HTML>";
+        return $res . "</div></HTML>";
     }
-
-
 }
